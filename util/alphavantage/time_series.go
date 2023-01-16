fad59496c1b585aca95a9bcb8257016ce033e81a
rbajollari <rbajollari@gmail.com>
2023-01-16 16:31:46 -0500

    Remove unused parseInt

--- util/alphavantage/time_series.go
+++ util/alphavantage/time_series.go
@@ -133,16 +133,6 @@ func parseFloat(val string) (float64, error) {
 	return strconv.ParseFloat(val, 64)
 }
 
-// parseInt parses an int value.
-// An error is returned if the value is not an int value.
-func parseInt(val string) (int, error) {
-	i, err := strconv.ParseInt(val, 10, 64)
-	if err != nil {
-		return 0, err
-	}
-	return int(i), nil
-}
-
 // parseDate parses a date value from a string.
 // An error is returned if the value is not in one of the dateFormat formats.
 func parseDate(v string, dateFormat ...string) (time.Time, error) {
