03e4af9baa193d031bce8bff6c2642d24d6e28f9
zarazan <zarazan@users.noreply.github.com>
2023-02-21 10:16:53 -0700

    add comment

--- config/config.go
+++ config/config.go
@@ -172,6 +172,7 @@ func ParseConfig(configPath string) (Config, error) {
 	}
 
 	viper.AutomaticEnv()
+	// Allow nested env vars to be read with underscore separators.
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.SetConfigFile(configPath)
 
