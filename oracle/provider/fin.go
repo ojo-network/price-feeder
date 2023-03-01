630a88e85f7d9a74a01f6b0129bac054a4940fc9
Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
2023-03-01 10:38:38 -0800

    lint, spacing, comments

--- oracle/provider/fin.go
+++ oracle/provider/fin.go
@@ -3,7 +3,7 @@ package provider
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"strings"
 	"time"
@@ -32,7 +32,6 @@ type (
 	FinTickers struct {
 		Tickers []FinTicker `json:"tickers"`
 	}
-
 	FinTicker struct {
 		Base   string `json:"base_currency"`
 		Target string `json:"target_currency"`
@@ -44,7 +43,6 @@ type (
 	FinCandles struct {
 		Candles []FinCandle `json:"candles"`
 	}
-
 	FinCandle struct {
 		Bin    string `json:"bin"`
 		Close  string `json:"close"`
@@ -54,7 +52,6 @@ type (
 	FinPairs struct {
 		Pairs []FinPair `json:"pairs"`
 	}
-
 	FinPair struct {
 		Base    string `json:"base"`
 		Target  string `json:"target"`
@@ -63,6 +60,7 @@ type (
 	}
 )
 
+// NewFinProvider returns a new instance of the Fin Provider.
 func NewFinProvider(endpoint Endpoint) *FinProvider {
 	if endpoint.Name == ProviderFin {
 		return &FinProvider{
@@ -76,26 +74,34 @@ func NewFinProvider(endpoint Endpoint) *FinProvider {
 	}
 }
 
-func (p FinProvider) GetTickerPrices(pairs ...types.CurrencyPair) (map[string]types.TickerPrice, error) {
+// GetTickerPrices queries the FIN json API and returns with a
+// map of string => types.TickerPrice.
+func (p FinProvider) GetTickerPrices(pairs ...types.CurrencyPair) (
+	map[string]types.TickerPrice, error,
+) {
 	path := fmt.Sprintf("%s%s", p.baseURL, finTickersEndpoint)
 	tickerResponse, err := p.client.Get(path)
 	if err != nil {
 		return nil, fmt.Errorf("FIN tickers request failed: %w", err)
 	}
 	defer tickerResponse.Body.Close()
-	tickerContent, err := ioutil.ReadAll(tickerResponse.Body)
+
+	tickerContent, err := io.ReadAll(tickerResponse.Body)
 	if err != nil {
-		return nil, fmt.Errorf("FIN tickers response read failed: %w", err)
+		return nil,
+			fmt.Errorf("FIN tickers response read failed: %w", err)
 	}
 	var tickers FinTickers
 	err = json.Unmarshal(tickerContent, &tickers)
 	if err != nil {
-		return nil, fmt.Errorf("FIN tickers response unmarshal failed: %w", err)
+		return nil,
+			fmt.Errorf("FIN tickers response unmarshal failed: %w", err)
 	}
 	tickerSymbolPairs := make(map[string]types.CurrencyPair, len(pairs))
 	for _, pair := range pairs {
 		tickerSymbolPairs[pair.Base+"_"+pair.Quote] = pair
 	}
+
 	tickerPrices := make(map[string]types.TickerPrice, len(pairs))
 	for _, ticker := range tickers.Tickers {
 		pair, ok := tickerSymbolPairs[strings.ToUpper(ticker.Symbol)]
@@ -105,33 +111,44 @@ func (p FinProvider) GetTickerPrices(pairs ...types.CurrencyPair) (map[string]ty
 		}
 		_, ok = tickerPrices[pair.String()]
 		if ok {
-			return nil, fmt.Errorf("FIN tickers response contained duplicate: %s", ticker.Symbol)
+			return nil,
+				fmt.Errorf("FIN tickers response contained duplicate: %s", ticker.Symbol)
 		}
 		tickerPrices[pair.String()] = types.TickerPrice{
 			Price:  sdk.MustNewDecFromStr(ticker.Price),
 			Volume: sdk.MustNewDecFromStr(ticker.Volume),
 		}
 	}
+
 	for _, pair := range pairs {
 		_, ok := tickerPrices[pair.String()]
 		if !ok {
-			return nil, fmt.Errorf("FIN ticker price missing for pair: %s", pair.String())
+			return nil,
+				fmt.Errorf("FIN ticker price missing for pair: %s", pair.String())
 		}
 	}
 	return tickerPrices, nil
 }
 
-func (p FinProvider) GetCandlePrices(pairs ...types.CurrencyPair) (map[string][]types.CandlePrice, error) {
+// GetCandlePrices queries the FIN json API and returns with a
+// map of string => []types.CandlePrice.
+func (p FinProvider) GetCandlePrices(pairs ...types.CurrencyPair) (
+	map[string][]types.CandlePrice, error,
+) {
 	pairAddresses, err := p.getFinPairAddresses()
 	if err != nil {
-		return nil, fmt.Errorf("FIN pair addresses lookup failed: %w", err)
+		return nil,
+			fmt.Errorf("FIN pair addresses lookup failed: %w", err)
 	}
+
 	candlePricesPairs := make(map[string][]types.CandlePrice)
 	for _, pair := range pairs {
 		address, ok := pairAddresses[pair.String()]
 		if !ok {
-			return nil, fmt.Errorf("FIN contract address lookup failed for pair: %s", pair.String())
+			return nil,
+				fmt.Errorf("FIN contract address lookup failed for pair: %s", pair.String())
 		}
+
 		candlePricesPairs[pair.String()] = []types.CandlePrice{}
 		windowEndTime := time.Now()
 		windowStartTime := windowEndTime.Add(-finCandleWindowSizeHours * time.Hour)
@@ -148,7 +165,8 @@ func (p FinProvider) GetCandlePrices(pairs ...types.CurrencyPair) (map[string][]
 			return nil, fmt.Errorf("FIN candles request failed: %w", err)
 		}
 		defer candlesResponse.Body.Close()
-		candlesContent, err := ioutil.ReadAll(candlesResponse.Body)
+
+		candlesContent, err := io.ReadAll(candlesResponse.Body)
 		if err != nil {
 			return nil, fmt.Errorf("FIN candles response read failed: %w", err)
 		}
@@ -157,6 +175,7 @@ func (p FinProvider) GetCandlePrices(pairs ...types.CurrencyPair) (map[string][]
 		if err != nil {
 			return nil, fmt.Errorf("FIN candles response unmarshal failed: %w", err)
 		}
+
 		candlePrices := []types.CandlePrice{}
 		for _, candle := range candles.Candles {
 			timeStamp, err := binToTimeStamp(candle.Bin)
@@ -169,11 +188,14 @@ func (p FinProvider) GetCandlePrices(pairs ...types.CurrencyPair) (map[string][]
 				TimeStamp: timeStamp,
 			})
 		}
+
 		candlePricesPairs[pair.String()] = candlePrices
 	}
 	return candlePricesPairs, nil
 }
 
+// GetAvailablePairs queries fin's pairs and returns a map of
+// pair => empty struct.
 func (p FinProvider) GetAvailablePairs() (map[string]struct{}, error) {
 	finPairs, err := p.getFinPairs()
 	if err != nil {
@@ -190,6 +212,8 @@ func (p FinProvider) GetAvailablePairs() (map[string]struct{}, error) {
 	return availablePairs, nil
 }
 
+// getFinPairs queries the fin json API for available pairs,
+// parses it, and returns it.
 func (p FinProvider) getFinPairs() (FinPairs, error) {
 	path := fmt.Sprintf("%s%s", p.baseURL, finPairsEndpoint)
 	pairsResponse, err := p.client.Get(path)
@@ -205,6 +229,8 @@ func (p FinProvider) getFinPairs() (FinPairs, error) {
 	return pairs, nil
 }
 
+// getFinPairAddresses queries the fin API for token pairs,
+// and returns a map of [base+quote] => pool id address.
 func (p FinProvider) getFinPairAddresses() (map[string]string, error) {
 	finPairs, err := p.getFinPairs()
 	if err != nil {
@@ -218,10 +244,10 @@ func (p FinProvider) getFinPairAddresses() (map[string]string, error) {
 }
 
 // SubscribeCurrencyPairs performs a no-op since fin does not use websockets
-func (p FinProvider) SubscribeCurrencyPairs(pairs ...types.CurrencyPair) {
-	return
-}
+func (p FinProvider) SubscribeCurrencyPairs(_ ...types.CurrencyPair) {}
 
+// binToTimeStamp takes a bin time expressed in a string
+// and converts it into a unix timestamp.
 func binToTimeStamp(bin string) (int64, error) {
 	timeParsed, err := time.Parse(time.RFC3339, bin)
 	if err != nil {
