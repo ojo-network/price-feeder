9f0c3611abe923e1c9d20a952af7689ac42c6ab6
Kyle Zarazan <zarazan@users.noreply.github.com>
2023-06-09 14:10:56 -0600

    add conversion tests

--- oracle/convert_test.go
+++ oracle/convert_test.go
@@ -6,6 +6,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ojo-network/price-feeder/oracle"
 	"github.com/ojo-network/price-feeder/oracle/types"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestConvertRatesToUSD(t *testing.T) {
@@ -38,4 +39,112 @@ func TestConvertRatesToUSD(t *testing.T) {
 			t.Errorf("Unexpected converted rate for currency pair: %v. Expected: %s, Got: %s", cp, expectedRate.String(), convertedRate.String())
 		}
 	}
+}
+
+func TestConvertAggregatedCandles(t *testing.T) {
+
+	candles := types.AggregatedProviderCandles{
+		"Provider1": types.CurrencyPairCandles{
+			types.CurrencyPair{Base: "ATOM", Quote: "USDC"}: []types.CandlePrice{
+				{Price: sdk.MustNewDecFromStr("35"), Volume: sdk.MustNewDecFromStr("1000"), TimeStamp: 1},
+				{Price: sdk.MustNewDecFromStr("40"), Volume: sdk.MustNewDecFromStr("1500"), TimeStamp: 2},
+			},
+			types.CurrencyPair{Base: "UMEE", Quote: "USDC"}: []types.CandlePrice{
+				{Price: sdk.MustNewDecFromStr("18"), Volume: sdk.MustNewDecFromStr("500"), TimeStamp: 1},
+				{Price: sdk.MustNewDecFromStr("22"), Volume: sdk.MustNewDecFromStr("800"), TimeStamp: 2},
+			},
+		},
+		"Provider2": types.CurrencyPairCandles{
+			types.CurrencyPair{Base: "ATOM", Quote: "USDT"}: []types.CandlePrice{
+				{Price: sdk.MustNewDecFromStr("30"), Volume: sdk.MustNewDecFromStr("800"), TimeStamp: 1},
+				{Price: sdk.MustNewDecFromStr("35"), Volume: sdk.MustNewDecFromStr("1000"), TimeStamp: 2},
+			},
+			types.CurrencyPair{Base: "JUNO", Quote: "USDT"}: []types.CandlePrice{
+				{Price: sdk.MustNewDecFromStr("5"), Volume: sdk.MustNewDecFromStr("200"), TimeStamp: 1},
+				{Price: sdk.MustNewDecFromStr("6"), Volume: sdk.MustNewDecFromStr("300"), TimeStamp: 2},
+			},
+		},
+	}
+
+	rates := types.CurrencyPairDec{
+		types.CurrencyPair{Base: "USDT", Quote: "USD"}: sdk.MustNewDecFromStr("2"),
+		types.CurrencyPair{Base: "USDC", Quote: "USD"}: sdk.MustNewDecFromStr("1"),
+	}
+
+	expectedResult := types.AggregatedProviderCandles{
+		"Provider1": types.CurrencyPairCandles{
+			types.CurrencyPair{Base: "ATOM", Quote: "USD"}: []types.CandlePrice{
+				{Price: sdk.MustNewDecFromStr("35"), Volume: sdk.MustNewDecFromStr("1000"), TimeStamp: 1},
+				{Price: sdk.MustNewDecFromStr("40"), Volume: sdk.MustNewDecFromStr("1500"), TimeStamp: 2},
+			},
+			types.CurrencyPair{Base: "UMEE", Quote: "USD"}: []types.CandlePrice{
+				{Price: sdk.MustNewDecFromStr("18"), Volume: sdk.MustNewDecFromStr("500"), TimeStamp: 1},
+				{Price: sdk.MustNewDecFromStr("22"), Volume: sdk.MustNewDecFromStr("800"), TimeStamp: 2},
+			},
+		},
+		"Provider2": types.CurrencyPairCandles{
+			types.CurrencyPair{Base: "ATOM", Quote: "USD"}: []types.CandlePrice{
+				{Price: sdk.MustNewDecFromStr("60"), Volume: sdk.MustNewDecFromStr("800"), TimeStamp: 1},
+				{Price: sdk.MustNewDecFromStr("70"), Volume: sdk.MustNewDecFromStr("1000"), TimeStamp: 2},
+			},
+			types.CurrencyPair{Base: "JUNO", Quote: "USD"}: []types.CandlePrice{
+				{Price: sdk.MustNewDecFromStr("10"), Volume: sdk.MustNewDecFromStr("200"), TimeStamp: 1},
+				{Price: sdk.MustNewDecFromStr("12"), Volume: sdk.MustNewDecFromStr("300"), TimeStamp: 2},
+			},
+		},
+	}
+
+	result := oracle.ConvertAggregatedCandles(candles, rates)
+
+	assert.Equal(t, expectedResult, result, "The converted candles do not match the expected result.")
+}
+
+func TestConvertAggregatedTickers(t *testing.T) {
+
+	tickers := types.AggregatedProviderPrices{
+		"Provider1": types.CurrencyPairTickers{
+			types.CurrencyPair{Base: "ATOM", Quote: "USDC"}: types.TickerPrice{
+				Price: sdk.MustNewDecFromStr("35"), Volume: sdk.MustNewDecFromStr("1000"),
+			},
+			types.CurrencyPair{Base: "UMEE", Quote: "USDC"}: types.TickerPrice{
+				Price: sdk.MustNewDecFromStr("18"), Volume: sdk.MustNewDecFromStr("500"),
+			},
+		},
+		"Provider2": types.CurrencyPairTickers{
+			types.CurrencyPair{Base: "ATOM", Quote: "USDT"}: types.TickerPrice{
+				Price: sdk.MustNewDecFromStr("30"), Volume: sdk.MustNewDecFromStr("800"),
+			},
+			types.CurrencyPair{Base: "JUNO", Quote: "USDT"}: types.TickerPrice{
+				Price: sdk.MustNewDecFromStr("5"), Volume: sdk.MustNewDecFromStr("200"),
+			},
+		},
+	}
+
+	rates := types.CurrencyPairDec{
+		types.CurrencyPair{Base: "USDT", Quote: "USD"}: sdk.MustNewDecFromStr("2"),
+		types.CurrencyPair{Base: "USDC", Quote: "USD"}: sdk.MustNewDecFromStr("1"),
+	}
+
+	expectedResult := types.AggregatedProviderPrices{
+		"Provider1": types.CurrencyPairTickers{
+			types.CurrencyPair{Base: "ATOM", Quote: "USD"}: types.TickerPrice{
+				Price: sdk.MustNewDecFromStr("35"), Volume: sdk.MustNewDecFromStr("1000"),
+			},
+			types.CurrencyPair{Base: "UMEE", Quote: "USD"}: types.TickerPrice{
+				Price: sdk.MustNewDecFromStr("18"), Volume: sdk.MustNewDecFromStr("500"),
+			},
+		},
+		"Provider2": types.CurrencyPairTickers{
+			types.CurrencyPair{Base: "ATOM", Quote: "USD"}: types.TickerPrice{
+				Price: sdk.MustNewDecFromStr("60"), Volume: sdk.MustNewDecFromStr("800"),
+			},
+			types.CurrencyPair{Base: "JUNO", Quote: "USD"}: types.TickerPrice{
+				Price: sdk.MustNewDecFromStr("10"), Volume: sdk.MustNewDecFromStr("200"),
+			},
+		},
+	}
+
+	result := oracle.ConvertAggregatedTickers(tickers, rates)
+
+	assert.Equal(t, expectedResult, result, "The converted tickers do not match the expected result.")
 }
