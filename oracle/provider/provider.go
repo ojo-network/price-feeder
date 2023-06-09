db058f97da2a1122594e0173fed92403b3ec27ff
Kyle Zarazan <zarazan@users.noreply.github.com>
2023-06-09 10:44:19 -0600

    move test currency pairs

--- oracle/provider/provider.go
+++ oracle/provider/provider.go
@@ -27,19 +27,6 @@ const (
 
 var (
 	ping = []byte("ping")
-
-	OJOUSDT  = types.CurrencyPair{Base: "OJO", Quote: "USDT"}
-	BTCUSDT  = types.CurrencyPair{Base: "BTC", Quote: "USDT"}
-	ATOMUSDT = types.CurrencyPair{Base: "ATOM", Quote: "USDT"}
-	LUNAUSDT = types.CurrencyPair{Base: "LUNA", Quote: "USDT"}
-
-	ATOMUSDC = types.CurrencyPair{Base: "ATOM", Quote: "USDC"}
-
-	OSMOATOM = types.CurrencyPair{Base: "OSMO", Quote: "ATOM"}
-	BCREATOM = types.CurrencyPair{Base: "BCRE", Quote: "ATOM"}
-
-	EURUSD = types.CurrencyPair{Base: "EUR", Quote: "USD"}
-	JPYUSD = types.CurrencyPair{Base: "JPY", Quote: "USD"}
 )
 
 type (
