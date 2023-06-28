a0c2631eec1f3025a6753b1cf0ea3677fef1f4fa
ryanbajollari <54822716+rbajollari@users.noreply.github.com>
2023-06-28 16:22:30 +0100

    feat: add kujira api provider (#157)
    * add kujira api provider
    
    * working with local kujira api
    
    * adapt kujira provider to refactor
    
    * bring back osmo api endpoint place holder for unit test
    
    * add kujira endpoint
    
    * config
    
    ---------
    
    Co-authored-by: Adam Wozniak <29418299+adamewozniak@users.noreply.github.com>
    (cherry picked from commit b6aa0473eb9b5bb6d091be25d999cfc0409b3087)
    
    # Conflicts:
    #	config/supported_assets.go
    #	oracle/provider/provider.go
    #	price-feeder.example.toml

--- oracle/provider/provider.go
+++ oracle/provider/provider.go
@@ -9,6 +9,7 @@ import (
 const (
 	defaultTimeout = 10 * time.Second
 
+<<<<<<< HEAD
 	ProviderKraken    types.ProviderName = "kraken"
 	ProviderBinance   types.ProviderName = "binance"
 	ProviderBinanceUS types.ProviderName = "binanceus"
@@ -23,6 +24,24 @@ const (
 	ProviderPolygon   types.ProviderName = "polygon"
 	ProviderCrescent  types.ProviderName = "crescent"
 	ProviderMock      types.ProviderName = "mock"
+=======
+	ProviderKraken     types.ProviderName = "kraken"
+	ProviderBinance    types.ProviderName = "binance"
+	ProviderBinanceUS  types.ProviderName = "binanceus"
+	ProviderOsmosisV2  types.ProviderName = "osmosisv2"
+	ProviderHuobi      types.ProviderName = "huobi"
+	ProviderOkx        types.ProviderName = "okx"
+	ProviderGate       types.ProviderName = "gate"
+	ProviderCoinbase   types.ProviderName = "coinbase"
+	ProviderBitget     types.ProviderName = "bitget"
+	ProviderMexc       types.ProviderName = "mexc"
+	ProviderCrypto     types.ProviderName = "crypto"
+	ProviderPolygon    types.ProviderName = "polygon"
+	ProviderCrescent   types.ProviderName = "crescent"
+	ProviderEthUniswap types.ProviderName = "eth-uniswap"
+	ProviderKujira     types.ProviderName = "kujira"
+	ProviderMock       types.ProviderName = "mock"
+>>>>>>> b6aa047 (feat: add kujira api provider (#157))
 )
 
 var (
