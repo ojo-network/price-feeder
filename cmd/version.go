2c02c7114bb8b5042c65a37b073219dbcd9af876
Kyle Zarazan <zarazan@users.noreply.github.com>
2023-08-10 12:14:10 -0600

    add back comment

--- cmd/version.go
+++ cmd/version.go
@@ -20,6 +20,7 @@ var (
 	// Commit defines the application commit hash (defined at compile time)
 	Commit = ""
 
+	// SDKVersion defines the cosmos sdk version (defined at compile time)
 	SDKVersion = ""
 
 	versionFormat string
