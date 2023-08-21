356e11e8e46edd9137d579ba34e6398733d743b8
rbajollari <rbajollari@gmail.com>
2023-08-21 15:20:09 -0400

    go through log messages

--- oracle/oracle.go
+++ oracle/oracle.go
@@ -245,7 +245,7 @@ func (o *Oracle) SetPrices(ctx context.Context) error {
 	}
 
 	if err := g.Wait(); err != nil {
-		o.logger.Info().Err(err).Msg("failed to get prices from provider")
+		o.logger.Error().Err(err).Msg("failed to get prices from provider")
 	}
 
 	computedPrices, err := o.GetComputedPrices(
@@ -258,7 +258,7 @@ func (o *Oracle) SetPrices(ctx context.Context) error {
 
 	for cp := range requiredRates {
 		if _, ok := computedPrices[cp]; !ok {
-			o.logger.Warn().Str("asset", cp.String()).Msg("unable to report price for expected asset")
+			o.logger.Error().Str("asset", cp.String()).Msg("unable to report price for expected asset")
 		}
 	}
 
